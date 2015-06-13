19b429bccb6ba34316d4660975b80b8cb6ed026a
Cam <cam@mackay.net.nz>
2015-06-13 21:59:05 +1200

    added grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,9 @@ module.exports = function(grunt) {
 
 	// Load the task plugins
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-stylus');
@@ -18,30 +19,45 @@ module.exports = function(grunt) {
 		console.log('Bazz is ' + baz);
 	});
 
-	// Project configuration.
+	// Project configuration
   	grunt.initConfig({
   		bar: {
   			foo: 42
   		},
+  		// should this banner be a separate grunt plugin?
+  		pkg: grunt.file.readJSON('package.json'),
+  		banner: '/*! <%= pkg.name %> -v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+  		'* <%= pkg.homepage %>\n' +
+  		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
     	jshint: {
       		options: {
         		curly: true,
         		eqeqeq: true
       		},
-      		target1: ['Gruntfile.js', 'src/**/*.js']
+      		target1: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     	},
-    	uglify: {
-	      target1: {
-	        src: 'src/foo.js',
-	        dest: 'build/foo.min.js'
-	      }
-	    },
+    	clean: {
+    		src: ['build']
+    	},
 	    concat: {
+	    	options: {
+	    		banner: '<%= banner %>',
+	    		stripBanners: true
+	    	},
 	    	target1: {
 	    		files: {
 	    			"build/abc.js": ["src/a.js", "src/b.js", "src/c.js"]
 	    		}
 	    	}
+	    },
+    	uglify: {
+    		options: {
+    			banner: '<%= banner %>'
+    		},
+	      target1: {
+	        src: 'build/abc.js',
+	        dest: 'build/abc.min.js'
+	      }
 	    },
 	    coffee: {
 	    	target1: {
@@ -76,6 +92,6 @@ module.exports = function(grunt) {
   	});
 
   	// Define the default task
-	grunt.registerTask('default', ['jshint', 'uglify', 'concat', 'coffee', 'jade', 'stylus']);
+	grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify', 'coffee', 'jade', 'stylus']);
 
 };
--- src/c.js
+++ src/c.js
@@ -1,1 +1,11 @@
-var c = 3;
+(function foo() {
+
+	var aLongVariableName = 42;
+
+	function aLongFunctionName(aLongParameterName) {
+		return aLongParameterName + 21;
+	}
+
+	alert(aLongFunctionName(aLongVariableName));
+
+})();
